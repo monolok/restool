af5c041884627e0b717250ccd9f0cb562fcfdc2b
monolok <antoinebertin@me.com>
2014-10-18 15:01:05 +0200

    new php coming up bitches !!

--- operation.php
+++ operation.php
@@ -1,22 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>operation</title>
-	<meta charset="utf-8">
-  	<link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,500,300' rel='stylesheet' type='text/css'>
-  	<link href='http://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
-  	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
- 	<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
- 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- Leave those next 4 lines if you care about users using IE8 -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-  	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-</head>
+<?php include('header.php'); ?>
 <body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -55,4 +39,5 @@ APP
 </div>
 
 </body>
+<?php include('footer.php'); ?>
 </html>
